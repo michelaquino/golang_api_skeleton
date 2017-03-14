cc7c9a65d38539efcca21c6de96e299f7aa2ce34
Michel Aquino <michel.aquino@corp.globo.com>
2017-03-14 19:29:17 -0300

    Add API error

--- api_errors/api_errors.go
+++ api_errors/api_errors.go
@@ -5,4 +5,10 @@ import "errors"
 var (
 	// ErrUnexpected represents an unexpected error
 	ErrUnexpected = errors.New("An unexpected error as occur")
+
+	// ErrNotFoundOnCache represents an error when the key was not found on cache
+	ErrNotFoundOnCache = errors.New("Not found on cache")
+
+	// ErrGetCacheValue represents an error when an error occur when get cache value
+	ErrGetCacheValue = errors.New("Not found on cache")
 )
