dec20e7543e739ff3ee7418cc89046f841659413
Michel Aquino <michel.has@gmail.com>
2017-03-21 23:51:27 -0300

    Using config struct on mongodb config

--- context/database.go
+++ context/database.go
@@ -2,10 +2,7 @@ package context
 
 import (
 	"fmt"
-	"os"
-	"strconv"
 	"sync"
-	"time"
 
 	mgo "gopkg.in/mgo.v2"
 )
@@ -29,43 +26,17 @@ func GetMongoSession() *mgo.Session {
 }
 
 func getNewMongoSession() (*mgo.Session, error) {
-	mongoURL := os.Getenv("MONGO_URL")
-	mongoPort := getMongoPort()
-	mongoTimeout := getMongoTimeout()
-	mongoAddress := fmt.Sprintf("%s:%d", mongoURL, mongoPort)
-
-	mongoDatabaseName := os.Getenv("MONGO_DATABASE_NAME")
-	mongoUserName := os.Getenv("MONGO_DATABASE_USERNAME")
-	mongoPassword := os.Getenv("MONGO_DATABASE_PASSWORD")
-
+	apiConfig := GetAPIConfig()
 	mongoDBDialInfo := &mgo.DialInfo{
 		Addrs: []string{
-			mongoAddress,
+			apiConfig.MongoDBConfig.Address,
 		},
-		Database: mongoDatabaseName,
-		Username: mongoUserName,
-		Password: mongoPassword,
-		Timeout:  mongoTimeout,
+		Database: apiConfig.MongoDBConfig.DatabaseName,
+		Timeout:  apiConfig.MongoDBConfig.Timeout,
+		Username: apiConfig.MongoDBConfig.Username,
+		Password: apiConfig.MongoDBConfig.Password,
 	}
 
 	session, err := mgo.DialWithInfo(mongoDBDialInfo)
 	return session, err
-}
-
-func getMongoPort() int {
-	mongoPort, err := strconv.Atoi(os.Getenv("MONGO_PORT"))
-	if err != nil {
-		return 27017
-	}
-
-	return mongoPort
-}
-
-func getMongoTimeout() time.Duration {
-	mongoTimeout, err := strconv.Atoi(os.Getenv("MONGO_TIMEOUT"))
-	if err != nil {
-		return time.Duration(60) * time.Second
-	}
-
-	return time.Duration(mongoTimeout) * time.Second
 }
