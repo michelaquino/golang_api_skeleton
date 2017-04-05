a74fe09c3b64072f313212d4954c01add7ee44fb
Michel Aquino <michelaquino@mailbox.org>
2017-04-04 22:23:25 -0300

    Add new relic config

---
+++ middleware/new_relic.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"bytes"
+	"io/ioutil"
+
+	"github.com/labstack/echo"
+	newrelic "github.com/newrelic/go-agent"
+)
+
+// NewRelicMiddleware is a middleware to send request info to New Relic
+func NewRelicMiddleware(newrelicApp newrelic.Application) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(echoContext echo.Context) error {
+			responseWriter := echoContext.Response().Writer()
+
+			// Copy struct request to remove body
+			requestCopy := *echoContext.Request()
+
+			// Set body empty to send to New Relic
+			requestCopy.Body = ioutil.NopCloser(bytes.NewReader([]byte{}))
+
+			transaction := newrelicApp.StartTransaction(requestCopy.URL.Path, responseWriter, &requestCopy)
+			defer transaction.End()
+
+			return next(echoContext)
+		}
+	}
+}
